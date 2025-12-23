2ef3b92560fc51105777082be0c5005f52173574
dariyatum <dariyatum412@gmail.com>
2025-12-23 16:35:21 +0700

    first commit

---
+++ themes/my-theme/header.php
@@ -0,0 +1,14 @@
+<head>
+    <link rel="stylesheet" href="/wp-content/themes/my-theme/style.css">
+</head>
+<header>
+    <h1 class="logo">logo</h1>
+    <div class="menu">
+        <li><a href="">home</a></li>
+        <li><a href="">about</a></li>
+        <li><a href="">contact</a></li>
+    </div>
+</header>
+<main>
+    
+</main>
